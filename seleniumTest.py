4caee5a545190131a63ee8896fa4a27a7674f93c
Mohamed Adss <m_adss@hotmail.com>
2024-03-18 08:36:50 +0200

    Update seleniumTest.py

--- seleniumTest.py
+++ seleniumTest.py
@@ -1,18 +1,17 @@
 from selenium import webdriver
 import time
+import sys
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
-s=Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=s)
+chromeDriverPath = sys.argv[1]
+driver = webdriver.Chrome(chromeDriverPath)
 
+url = 'https://www.google.com/'
+xpath = '//*[@id="SIvCob"]'
 for i in range(3):
     driver.get(url)
     time.sleep(3)
     price = driver.find_element("xpath", xpath)
-    price_int = float(price.get_attribute("innerHTML")[1:])
-    print(price_int)
-    if price_int <= my_price:
-        print('price dropped!!!')
-        
-    time.sleep(wait)
+    price_html = price.get_attribute("innerHTML")
+    print(price_html)
